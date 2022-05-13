4df9d40a11c9a0e5abebd979b6a21301e55989d4
Mikhail <a104s@mail.ru>
2022-05-13 12:21:46 +0300

    Task 2 Variant 2

---
+++ Task2_Var2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите число : ");
+int num = int.Parse(Console.ReadLine());
+int res = 0;
+if (num<99)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (num > 99)
+    {
+        res = num % 10;
+        num = num / 10;
+    }
+    Console.WriteLine(res);
+}
--- Tasl_2/Program.cs
+++ Tasl_2/Program.cs
@@ -2,4 +2,3 @@
 string s = Console.ReadLine();
 if (s.Length < 3) Console.WriteLine("Третьей цифры нет");
 else Console.WriteLine(s[2]);
-
